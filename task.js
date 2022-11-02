d98311bdec21d7ac78c32cededbbc17152f8448f
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-11-02 11:27:32 +0300

    corrected2

--- task.js
+++ task.js
@@ -1,81 +1,80 @@
 class AlarmClock {
-  constructor() {
+  constructor(timerId = null) {
     this.alarmCollection = [];
-    this.timerId;
   }
+
   addClock(time, callback, id) {
-    this.time = time;
-    this.callback = callback;
-    this.id = id;
     if (id === undefined) {
       throw new Error("error text");
     }
 
-    if (this.timerId) {
-      console.error();
+    if (this.alarmCollection.some((item) => item.id === id)) {
+      console.error("Ошибка!!! Звонок уже существует");
       return;
     }
-    this.alarmCollection.push({
-      id: this.id,
-      time: this.time,
-      callback: this.callback,
-    });
+
+    this.alarmCollection.push({ time: "time", callback: callback, id: id });
   }
+
   removeClock(id) {
-    try {
-      for (let i = 0; i < this.alarmCollection.length; i++) {
-        console.log(
-          this.alarmCollection.filter(() => this.alarmCollection[i].id === id)
-        ); // ???не понимаю как с помощью фильтра удалить объект из массива..? и что значит - "Верните логическое значение об успешности/провале удаления объекта звонка из общего массива."
-      }
-    } catch (error) {
-      return error;
-    }
+    let larr = this.alarmCollection.length;
+
+    this.alarmCollection = this.alarmCollection.filter(
+      (item) => item.id !== id
+    );
+    return larr === this.alarmCollection.length;
   }
-  getCurrentFormattedTime(time) {
-    this.time = String(time);
+
+  getCurrentFormattedTime() {
+    this.today = new Date();
+    return this.today.toUTCString();
   }
 
   start() {
-    function checkCliock(call) {
-      if (new Date() === this.alarmCollection[call].time) {
-        return this.alarmCollection[call].callback;
-      } //else if (this.alarmCollection[call].time === underfind) {
+    if (timerId != null) {
+      console.log("Интервал запущен");
+    } else if (timerId === null) {
+      timerId = this.alarmCollection.forEach(function r(item) {
+        if (item.time === item.getCurrentFormattedTime()) {
+          return this.alarmCollection.callback;
+        }
+      });
     }
-    /*запускает все звонки - что это значит???
-Создайте функцию проверки (checkClock), которая принимает звонок и проверяет: если текущее время совпадает со временем звонка, то вызывайте колбек.??? что значит "принимает звонок"? это значит в аргумент подставляется объект в массиве alarmCollection?
-Если значение идентификатора текущего таймера отсутствует, то создайте новый интервал.
-??? -  А что делать если текущее время не совпадает со временем звонка?
-В этом интервале реализуйте функцию, которая будет перебирать все звонки, и для каждого вызывать функцию checkClock. - не понимаю, внутри функции checkClock должна вызываться checkClock???
-Результат функции setInterval сохраните в свойстве идентификатора текущего таймера.*/
+    /* function checkCliock(call) {
+      if(new Date() === time)*/
+    /*В методе start должна быть следующая реализация. 
+    Вы должны проверить запущен ли уже интервал? Если интервал запущен, то в timerId должно быть какое-то значение. 
+    В таком случае ничего не нужно выполнять…Если итервал не запускался (в timerId отсутствует значение), то вы должны создать новый интервал.
+    В этом интервале, вы должны перебирать все объекты звонков (с помощью метода forEach) и каждый звонок необходимо проверить: необходимо ли ему прозвенеть? 
+    Если у звонка время (свойство time) совпадает с текущими временем (результатом метода getCurrentFormattedTime), то вы должны вызывать колбек звонка…
+    Для удобства проверки, в задании предлагается реализовать отдельную “мини” функцию checkClock, в которую будет передаваться звонок и этот звонок будет проверяться, 
+    а по необходимости вызываться. Только эту функцию советую реализовать стрелочным синтаксисом, чтобы не потерять контекст 
+    --- ??? Мне не понятно что значит запущенный интервал (значение timerId). Что значит создать новый интервал?*/
   }
-  stop() {
-    /*останавливает выполнение всех звонков
 
-Сделайте проверку существования идентификатора текущего таймера.
-Если у идентификатора текущего таймера есть значение, то вызовите функцию clearInterval для удаления интервала, а так же удалите значение из свойства "идентификатор текущего таймера". - ??? где эта функция clearInterval описана?*/
-  }
-  printAlarms() {
-    for (let i = 0; i < this.alarmCollection.length; i++) {
-      this.alarmCollection.forEach(() =>
-        console.log(
-          `id: ${this.alarmCollection[i].id};`,
-          `time: ${this.alarmCollection[i].time}.`
-        )
-      );
-      //С помощью метода forEach выведите информацию о каждом звонке (id и time). ??? как сделать чтобы при вызове меторда значения в консоли не дублировались?
+  stop(timerId) {
+    if (timerId !== null) {
+      this.alarmCollection.clearInterval(id);
     }
   }
+
+  printAlarms() {
+    this.alarmCollection.forEach((item) =>
+      console.log(`id: ${item.id};`, `time: ${item.time}.`)
+    );
+  }
+
   clearAlarms() {
-    stop(this.alarmCollection);
-    // с помощью какого метода необходимо удалять звонки? с помощью delete?
-    /*удаляет все звонки
-Вызывает метод остановки интервала.
-Удаляет все звонки.*/
+    this.stop();
+    this.alarmCollection = [];
   }
 }
 let a = new AlarmClock();
-a.addClock(42, 5, 3);
-a.addClock(4, 5, 4);
-a.removeClock(3);
+a.addClock(42, 5, 21);
+a.addClock(4, 5, 1);
+a.addClock(54, 5, 83);
+a.addClock(94, 5, 93);
+console.log(a.getCurrentFormattedTime());
+console.log(a.removeClock(15));
+console.log(a.alarmCollection);
 a.printAlarms();
